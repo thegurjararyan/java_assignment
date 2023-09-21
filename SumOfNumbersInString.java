5294482a6fd3b69a18d8506d6ba49e664ba254f7
Aryan Chaudhary <114920247+thegurjararyan@users.noreply.github.com>
2023-09-22 00:05:45 +0530

    last question remaining

---
+++ SumOfNumbersInString.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class SumOfNumbersInString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        
+        int sum = calculateSumOfNumbers(input);
+        
+        System.out.println("Input: " + input);
+        System.out.println("Output: " + sum);
+        
+    }
+
+    public static int calculateSumOfNumbers(String input) {
+        int sum = 0;
+        int currentNumber = 0;
+        boolean inNumber = false;
+
+        for (char c : input.toCharArray()) {
+            if (Character.isDigit(c)) {
+                currentNumber = currentNumber * 10 + (c - '0');
+                inNumber = true;
+            } else {
+                if (inNumber) {
+                    sum += currentNumber;
+                    currentNumber = 0;
+                    inNumber = false;
+                }
+            }
+        }
+        if (inNumber) {
+            sum += currentNumber;
+        }
+
+        return sum;
+    }
+}
